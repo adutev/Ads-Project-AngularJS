cd4519af4530c1c9adb056c14c28ca7eb201c72a
adutev <adutev@gmail.com>
2014-12-30 23:03:30 +0200

    Added getAllAds, getAllTowns, getAllCategories Homepage 40% done.

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,15 @@
-var adsApp = angular.module('adsApp', ['ngRoute']);
-adsApp.config(function($routeProvider) {
+var app = angular.module('app', ['ngRoute']);
+app.config(function($routeProvider) {
 	$routeProvider.when('/login', {
-		templateUrl:'templates/login.html'
+		templateUrl: 'templates/login.html'
 	});
 	$routeProvider.when('/register', {
-		templateUrl:'templates/register.html'
+		templateUrl: 'templates/register.html'
 	});
 	$routeProvider.when('/', {
-		templateUrl:'templates/home.html'
+		templateUrl: 'templates/home.html'
 	});
+	$routeProvider.otherwise({
+		redirectTo: '/'
+	})
 })
--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,5 +1,15 @@
-adsApp.controller('MainController', function($scope, $location) {
+app.controller('MainController', function($scope, $location, mainData) {
 	$scope.changeView = function(path) {
 		$location.path(path);
 	};
+
+	mainData.getAllAds(function(resp) {
+		$scope.data = resp;
+	});
+	mainData.getAllTowns(function(resp) {
+		$scope.towns = resp;
+	});
+	mainData.getAllCategories(function(resp) {
+		$scope.categories = resp;
+	})
 })
