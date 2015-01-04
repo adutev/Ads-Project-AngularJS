835a4994c61735a501f029f615bbda145942d2ed
sarfaa <regki@mail.bg>
2015-01-04 19:05:07 +0200

    Fixed refreshing

--- app/js/controllers/MainController.js
+++
@@ -1,6 +0,0 @@
-app.controller('MainController', function($scope, $location) {
-	// Looks after changing the views
-	$scope.changeView = function(path) {
-		$location.path(path);
-	};
-})
--- app/js/controllers/RegisterController.js
+++
@@ -1,5 +0,0 @@
-app.controller('RegisterController', function($http, $log, $q, baseUrl) {
-    $scope.login = function (loginDetails, loginForm) {
-        console.log(loginDetails.password)
-    }
-})
