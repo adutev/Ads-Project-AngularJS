e4f6935430883cf181b29e633f67745eda8c63ed
adutev <adutev@gmail.com>
2015-01-10 12:44:15 +0200

    Removed unnecessary files

--- app/js/controllers/RegisterController.js
+++ app/js/controllers/RegisterController.js
@@ -1,6 +1,6 @@
 'use strict'
 
-app.controller('RegisterController', ['$scope', '$rootScope', '$location', 'authorization', 'getTowns', 'notifications', function($scope, $rootScope, $location, authorization, getTowns, notifications) {
+app.controller('RegisterController', ['$scope', '$rootScope', '$location', 'authorization', 'getTowns', function($scope, $rootScope, $location, authorization, getTowns) {
   $scope.credentials = {
     username: '',
     password: ''
@@ -13,11 +13,18 @@ app.controller('RegisterController', ['$scope', '$rootScope', '$location', 'auth
 
   $scope.register = function(credentials) {
     authorization.register(credentials).then(function(response) {
+      console.log(response)
       $rootScope.$broadcast('registeredUser');
-      notifications.registerSuccess();
       $scope.changeRoute('/');
     }, function(error) {
-      notifications.registerFailed(error)
+      $rootScope.$broadcast('registeredFailed', error);
     });
   }
+
+  $rootScope.$on("registeredFailed", function(event, data) {
+    for (var i in data.modelState[""]) {
+      console.log(data.modelState[""][i]);
+
+    }
+  });
 }])
